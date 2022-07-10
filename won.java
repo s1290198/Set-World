682608780474ef46a88f994ca97c639726cd97de
s1290198 <s1290198@u-aizu.ac.jp>
2022-07-10 16:20:51 +0900

    Victory message added

--- won.java
+++ won.java
@@ -20,20 +20,19 @@ public class won{
 
 
 
-		int frag1=0,frag2=0;
-        System.out.println("Tossing a coin...");
 
-        for(int i=0;i<5;i++){
-            System.out.print("Round "+i+": ");
-            Random r = new Random();
-            int coin = r.nextInt(2);
-            if(coin==0){
-                ++frag1;
-                System.out.println("Heads");
-            }
-            else if(coin==1){
-                ++frag2;
-                System.out.println("Tails");
+
+
+
+
+
+
+
+
+
+
+
+
 
                 if(frag1>frag2) System.out.println("You won");
                 else System.out.println("You lost");
