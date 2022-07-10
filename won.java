a98bd2bc8c4cfad5be2ba5cbdf12379370e8fe00
s1290198 <s1290198@u-aizu.ac.jp>
2022-07-10 17:06:07 +0900

    Victory message added

--- won.java
+++ won.java
@@ -1,13 +1,8 @@
+import java.util.Scanner;
 import java.util.Random;
 
 public class won{
-        public static void main(String[] args){
-
-		
-
-
-
-
+    public static void main(String[] args){
 
 
 
