8db01fa41197409552319a02601dba631295242c
s1290198 <s1290198@u-aizu.ac.jp>
2022-07-10 14:50:31 +0900

    Victory message added

--- won.java
+++ won.java
@@ -1,5 +1,8 @@
 public class won{
         public static void main(String[] args){
+
+		int frag1=0,frag2=0;
+
                 if(frag1>frag2) System.out.println("You won");
                 else System.out.println("You lost");
         }
