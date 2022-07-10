3e437ccd7c563bf62ca42cc0472c48efc9c074e8
s1290198 <s1290198@u-aizu.ac.jp>
2022-07-10 14:19:34 +0900

    Victory message added

--- won.java
+++ won.java
@@ -1,2 +1,6 @@
-public class coin{
-	public static void main
+public class won{
+        public static void main(String[] args){
+                if(frag1>frag2) System.out.println("You won");
+                else System.out.println("You lost");
+        }
+}
