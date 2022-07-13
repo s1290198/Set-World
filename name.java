d8fb98df49c65044ae722d05505e72881e9193d1
s1290198 <s1290198@u-aizu.ac.jp>
2022-07-13 22:13:10 +0900

    Victory message added

---
+++ name.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+
+
+public class Coin{
+    public static void main(String[] args){
+
+        
+        
+
+        
+        
+
+        
+
+        int frag1=0,frag2=0;
+        System.out.println("Tossing a coin...");
+
+        for(int i=0;i<5;i++){
+            System.out.print("Round "+i+": ");
+            Random r = new Random();
+            int coin = r.nextInt(2);
+            if(coin==0){
+                ++frag1;
+                System.out.println("Heads");
+            }
+            else if(coin==1){
+                ++frag2;
+                System.out.println("Tails");
+            }
+        }
+        System.out.println("Heads: "+frag1+", Tails: "+frag2);
+
+	if(frag1>frag2) System.out.println("You won");
+        else System.out.println("You lost!");
+    }
+}
